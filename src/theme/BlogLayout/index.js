e339f3e5074127652d894ff8503654d84b05aa14
Mark Fomin <luring.uliksir@gmail.com>
2023-08-20 19:49:11 +0300

    Final comments fix

--- src/theme/BlogLayout/index.js
+++ src/theme/BlogLayout/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import BlogSidebar from '@theme/BlogSidebar';
 import Translate from '@docusaurus/Translate';
 import styles from './styles.module.css';
+
+function bindComments(telegramPost) {
+  useEffect(() => {
+    console.log("DOMContentLoaded");
+    let telegramScript = document.createElement("script");
+    telegramScript.setAttribute("src", "https://telegram.org/js/telegram-widget.js?22");
+    telegramScript.setAttribute("data-telegram-discussion", telegramPost);
+    telegramScript.setAttribute("data-comments-limit", "10");
+    telegramScript.setAttribute("data-color", "29B127");
+    telegramScript.setAttribute("data-dark-color", "72E350");
+    let mountCommentsNode = document.getElementById("mountComments");
+    mountCommentsNode.parentNode.insertBefore(telegramScript, mountCommentsNode);
+    console.log("Telegram script added");
+  })
+  return (<></>);
+}
 export default function BlogLayout(props) {
   const {sidebar, toc, children, ...layoutProps} = props;
   const hasSidebar = sidebar && sidebar.items.length > 0;
@@ -14,21 +30,13 @@ export default function BlogLayout(props) {
   );
   console.log("Telegram Post Linked", telegramPost);
   const comments = telegramPost ? (
-    <div id="mountComments"></div>
+    <>
+      <div id="mountComments"></div>
+      {bindComments(telegramPost)}
+    </>
   ) : (
     <div><Translate>Comments to this post are disabled</Translate></div>
   );
-  if (telegramPost) {
-    document.addEventListener("DOMContentLoaded", () => {
-      let telegramScript = document.createElement("script");
-      telegramScript.setAttribute("src", "https://telegram.org/js/telegram-widget.js?22");
-      telegramScript.setAttribute("data-telegram-discussion", telegramPost);
-      telegramScript.setAttribute("data-comments-limit", "10");
-      telegramScript.setAttribute("data-color", "29B127");
-      telegramScript.setAttribute("data-dark-color", "72E350");
-      document.getElementById("mountComments").insertBefore(telegramScript);
-    })
-  }
   return (
     <Layout {...layoutProps}>
       <div className="container margin-vert--lg">
