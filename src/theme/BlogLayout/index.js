b298f50f6616839a4a7e679e4c8d46b5405bb1db
Mark Fomin <luring.uliksir@gmail.com>
2023-08-20 18:06:56 +0300

    Improving blog comments 2

--- src/theme/BlogLayout/index.js
+++ src/theme/BlogLayout/index.js
@@ -7,7 +7,11 @@ import styles from './styles.module.css';
 export default function BlogLayout(props) {
   const {sidebar, toc, children, ...layoutProps} = props;
   const hasSidebar = sidebar && sidebar.items.length > 0;
-  const telegramPost = props.children?.[0].props?.items?.[0]?.content?.frontMatter?.telegram;
+  console.log(props);
+  const telegramPost = (
+    props.children?.[0].props?.items?.[0]?.content?.frontMatter?.telegram ||
+    props.children?.[0].props?.children?.type?.frontMatter?.telegram
+  );
   console.log("Telegram Post Linked", telegramPost);
   const comments = telegramPost ? (
     <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-discussion={telegramPost} data-comments-limit="5" data-color="29B127" data-dark-color="72E350"></script>
