d15ee86993291bda96a86ae8bd1d4abc7fe5a272
Mark Fomin <luring.uliksir@gmail.com>
2023-08-20 17:45:57 +0300

    Improving blog comments

--- src/theme/BlogLayout/index.js
+++ src/theme/BlogLayout/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import BlogSidebar from '@theme/BlogSidebar';
-import './styles.module.css';
+import Translate from '@docusaurus/Translate';
+import styles from './styles.module.css';
 export default function BlogLayout(props) {
   const {sidebar, toc, children, ...layoutProps} = props;
   const hasSidebar = sidebar && sidebar.items.length > 0;
+  const telegramPost = props.children?.[0].props?.items?.[0]?.content?.frontMatter?.telegram;
+  console.log("Telegram Post Linked", telegramPost);
+  const comments = telegramPost ? (
+    <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-discussion={telegramPost} data-comments-limit="5" data-color="29B127" data-dark-color="72E350"></script>
+  ) : (
+    <div><Translate>Comments to this post are disabled</Translate></div>
+  );
   return (
     <Layout {...layoutProps}>
       <div className="container margin-vert--lg">
@@ -19,8 +27,8 @@ export default function BlogLayout(props) {
             itemScope
             itemType="http://schema.org/Blog">
             {children}
-            <div className={clsx('telegramComments')}>
-            <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-discussion="onetimeusername2" data-comments-limit="5" data-color="29B127" data-dark-color="72E350"></script>
+            <div className={clsx(styles.telegramComments)}>
+              {comments}
             </div>
           </main>
           {toc && <div className="col col--2">{toc}</div>}
