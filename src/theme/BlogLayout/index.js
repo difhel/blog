c02bc2e963cb6793dff45493f6b1eb48e24faf5b
Mark Fomin <luring.uliksir@gmail.com>
2023-08-20 16:46:37 +0300

    Improving Telegram discussing widget styles

--- src/theme/BlogLayout/index.js
+++ src/theme/BlogLayout/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import BlogSidebar from '@theme/BlogSidebar';
+import './styles.module.css';
 export default function BlogLayout(props) {
   const {sidebar, toc, children, ...layoutProps} = props;
   const hasSidebar = sidebar && sidebar.items.length > 0;
@@ -18,11 +19,13 @@ export default function BlogLayout(props) {
             itemScope
             itemType="http://schema.org/Blog">
             {children}
+            <div className={clsx('telegramComments')}>
+            <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-discussion="onetimeusername2" data-comments-limit="5" data-color="29B127" data-dark-color="72E350"></script>
+            </div>
           </main>
           {toc && <div className="col col--2">{toc}</div>}
         </div>
       </div>
-      <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-discussion="onetimeusername2" data-comments-limit="5" data-color="29B127" data-dark-color="72E350"></script>
     </Layout>
   );
 }
