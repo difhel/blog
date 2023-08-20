3292985351f4e6caa09af5814c0fb8da3e9b8ff5
Mark Fomin <luring.uliksir@gmail.com>
2023-08-20 16:21:25 +0300

    Testing Telegram discussing widget

---
+++ src/theme/BlogLayout/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import BlogSidebar from '@theme/BlogSidebar';
+export default function BlogLayout(props) {
+  const {sidebar, toc, children, ...layoutProps} = props;
+  const hasSidebar = sidebar && sidebar.items.length > 0;
+  return (
+    <Layout {...layoutProps}>
+      <div className="container margin-vert--lg">
+        <div className="row">
+          <BlogSidebar sidebar={sidebar} />
+          <main
+            className={clsx('col', {
+              'col--7': hasSidebar,
+              'col--9 col--offset-1': !hasSidebar,
+            })}
+            itemScope
+            itemType="http://schema.org/Blog">
+            {children}
+          </main>
+          {toc && <div className="col col--2">{toc}</div>}
+        </div>
+      </div>
+      <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-discussion="onetimeusername2" data-comments-limit="5" data-color="29B127" data-dark-color="72E350"></script>
+    </Layout>
+  );
+}
