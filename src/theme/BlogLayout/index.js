0ad7f2cf4f84e4413dffa5fe46cf15e362dd95c3
Mark Fomin <luring.uliksir@gmail.com>
2024-03-09 23:09:09 +0300

    fix: fixed comments

--- src/theme/BlogLayout/index.js
+++ src/theme/BlogLayout/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import BlogSidebar from '@theme/BlogSidebar';
@@ -6,7 +6,10 @@ import Translate from '@docusaurus/Translate';
 import styles from './styles.module.css';
 
 function bindComments(telegramPost) {
+  const [DOMRendered, setDOMRendered] = useState(false);
   useEffect(() => {
+    if (DOMRendered) return;
+    setDOMRendered(true);
     console.log("DOMContentLoaded");
     let telegramScript = document.createElement("script");
     telegramScript.setAttribute("src", "https://telegram.org/js/telegram-widget.js?22");
@@ -20,13 +23,15 @@ function bindComments(telegramPost) {
   })
   return (<></>);
 }
+
 export default function BlogLayout(props) {
   const {sidebar, toc, children, ...layoutProps} = props;
   const hasSidebar = sidebar && sidebar.items.length > 0;
   console.log(props);
   const telegramPost = (
-    props.children?.[0].props?.items?.[0]?.content?.frontMatter?.telegram ||
-    props.children?.[0].props?.children?.type?.frontMatter?.telegram
+    // props.children?.[0].props?.items?.[0]?.content?.frontMatter?.telegram ||
+    props.children?.[1]?.props?.children?.type?.frontMatter?.telegram
+    // props.children?.[1].props?.children
   );
   console.log("Telegram Post Linked", telegramPost);
   const comments = telegramPost ? (
@@ -50,9 +55,13 @@ export default function BlogLayout(props) {
             itemScope
             itemType="http://schema.org/Blog">
             {children}
-            <div className={clsx(styles.telegramComments)}>
-              {comments}
-            </div>
+            {
+              location.href.endsWith("blog") ? null : (
+                <div className={clsx(styles.telegramComments)}>
+                  {comments}
+                </div>
+              )
+            }
           </main>
           {toc && <div className="col col--2">{toc}</div>}
         </div>
