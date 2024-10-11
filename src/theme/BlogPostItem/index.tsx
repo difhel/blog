09588a600752e24428d61c28a4bd1b69719a97da
Mark Fomin <mark@difhel.dev>
2024-10-11 10:02:33 +0300

    feat: introduce blog cuts

---
+++ src/theme/BlogPostItem/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import clsx from 'clsx';
+import { useBlogPost } from '@docusaurus/plugin-content-blog/client';
+import BlogPostItemContainer from '@theme/BlogPostItem/Container';
+import BlogPostItemHeader from '@theme/BlogPostItem/Header';
+import BlogPostItemContent from '@theme/BlogPostItem/Content';
+import BlogPostItemFooter from '@theme/BlogPostItem/Footer';
+import type { Props } from '@theme/BlogPostItem';
+
+// apply a bottom margin and add border in list view
+function useContainerClassName() {
+  const { isBlogPostPage } = useBlogPost();
+  return !isBlogPostPage ? 'margin-bottom--xl article-feed-item' : undefined;
+}
+
+export default function BlogPostItem({
+  children,
+  className,
+}: Props): JSX.Element {
+  const containerClassName = useContainerClassName();
+  const { isBlogPostPage, frontMatter, assets } = useBlogPost();
+  return (
+    <BlogPostItemContainer className={clsx(containerClassName, className)}>
+      <BlogPostItemHeader />
+      {isBlogPostPage && <BlogPostItemContent>{children}</BlogPostItemContent>}
+      {!isBlogPostPage && (
+        <>
+          <img src={assets.image} className="article-feed-image" alt={frontMatter.title} />
+          <div dangerouslySetInnerHTML={{ __html: (frontMatter as any).cutHtml }}></div>
+        </>
+      )}
+      <BlogPostItemFooter />
+    </BlogPostItemContainer>
+  );
+}
