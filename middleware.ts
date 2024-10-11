4a1a34da9149093e9124234fa7498fb0959811f8
Mark Fomin <78644136+difhel@users.noreply.github.com>
2024-10-11 12:30:13 +0000

    fix: finnaly, fix internal server error

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,8 @@ export default function middleware(request: Request) {
   if (url.pathname.startsWith("/ru")) {
     return undefined;
   }
-  console.log(JSON.stringify(request), JSON.stringify(request.headers));
-  return undefined;
-  const acceptLanguage = request.headers["Accept-Language"] as string;
+
+  const acceptLanguage = request.headers.get("Accept-Language") as string;
 
   if (acceptLanguage.includes('ru-RU')) {
     return rewrite(new URL('/ru' + url.pathname, url));
