2af52011375aa3146f93f2741a4accb89540ca2d
Mark Fomin <78644136+difhel@users.noreply.github.com>
2024-10-11 12:16:22 +0000

    feat: add middleware

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { rewrite } from '@vercel/edge';
+
+export default function middleware(request: Request) {
+  const url = new URL(request.url);
+  if (url.pathname.startsWith("/ru")) {
+    return undefined;
+  }
+  const acceptLanguage = request.headers["Accept-Language"] as string;
+
+  if (acceptLanguage.includes('ru-RU')) {
+    return rewrite(new URL('/ru' + url.pathname, url));
+  }
+ 
+  if (url.pathname.startsWith('/banana')) {
+    return rewrite(new URL('https://google.com'));
+  }
+}
