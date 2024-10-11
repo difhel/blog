9a5d3b0157a4c91e90d2a9f4c12884cdf6557f00
Mark Fomin <78644136+difhel@users.noreply.github.com>
2024-10-11 20:00:31 +0000

    redeploy

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,27 @@
 import { rewrite } from '@vercel/edge';
 
+function redirect(newUrl: string, baseUrl: URL) {
+  const key = `?redirect_key=${Math.random().toString(16).slice(2)}`;
+  return rewrite(new URL(newUrl + baseUrl.pathname + key, baseUrl), { status: 302 });
+}
+
 export default function middleware(request: Request) {
   const url = new URL(request.url);
 
-  if (url.pathname.startsWith("/ru/en") || url.pathname.startsWith("/en/ru")) {
-    // When user switch their language
-    url.pathname.replace("/ru/en", "");
-    url.pathname.replace("/en/ru", "");
-  }
   if (url.pathname.startsWith("/ru") || url.pathname.startsWith("/en")) {
     return undefined;
   }
 
   const acceptLanguage = request.headers.get("Accept-Language");
-  if (!acceptLanguage) return rewrite(new URL('/en' + url.pathname, url), { status: 302 });
+  if (!acceptLanguage) return redirect('/en?1', url);
 
   if (acceptLanguage.includes('ru-RU')) {
-    return rewrite(new URL('/ru' + url.pathname, url), { status: 302 });
+    return redirect('/ru', url);
   }
  
   if (url.pathname.startsWith('/z')) {
     return rewrite(new URL('https://службапоконтракту.рф'));
   }
 
-  return rewrite(new URL('/en' + url.pathname, url), { status: 302 });
+  return redirect('/en?2', url);
 }
