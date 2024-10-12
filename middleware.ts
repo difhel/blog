41f5d408e56967c72c38f0f7001316a369ac99da
Mark Fomin <78644136+difhel@users.noreply.github.com>
2024-10-12 12:03:36 +0300

    App: Auto language detection via Vercel Edge Middleware (#4)
    * feat: auto language detection
    
    * feat: add middleware
    
    * fix: fix internal server error
    
    * trying to JSON.stringify logs
    
    * fix: finnaly, fix internal server error
    
    * feat: new building scheme
    
    * fix: probably fix switching languages
    
    * feat: another way to achieve default locale
    
    * redeploy
    
    * redeploy
    
    * redeploy
    
    * redeploy
    
    * redeploy by gpt
    
    * redeploy by gpt 2
    
    * redeploy by gpt 3
    
    * new solution by edge middleware
    
    * fix: fix broken pages
    
    * feat: final changes
    
    * build: fix development build
    
    * build: remove unused dependencies

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { rewrite } from '@vercel/edge';
+
+function redirect(newUrl: string, baseUrl: URL) {
+  // const key = `?redirect_key=${Math.random().toString(16).slice(2)}`;
+  const key = '';
+  const redirectUrl = new URL(newUrl + baseUrl.pathname + key, baseUrl);
+  return rewrite(baseUrl, { status: 302, headers: { Location: redirectUrl.href } });
+}
+
+export default function middleware(request: Request) {
+  const url = new URL(request.url);
+
+  if (url.pathname.startsWith("/ru") || url.pathname.startsWith("/en")) {
+    return undefined;
+  }
+
+  const acceptLanguage = request.headers.get("Accept-Language");
+  if (!acceptLanguage) return redirect('/en', url);
+
+  if (acceptLanguage.includes('ru-RU')) {
+    return redirect('/ru', url);
+  }
+
+  return redirect('/en', url);
+}
