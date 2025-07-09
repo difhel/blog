0c9f18c598aeb07bc3f12490ee90d5d78f4fd811
Mark Fomin <mark@difhel.dev>
2025-07-10 00:35:52 +0300

    Update `BlogLayout` from upstream

--- src/theme/BlogLayout/index.tsx
+++ src/theme/BlogLayout/index.tsx
@@ -1,15 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import React, { useEffect, useRef, type ReactNode } from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import BlogSidebar from '@theme/BlogSidebar';
+
+import type { Props } from '@theme/BlogLayout';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+
 import styles from './styles.module.css';
 
 function bindComments(telegramPost) {
-  const [DOMRendered, setDOMRendered] = useState(false);
+  console.log("bindComments", telegramPost);
+  const hasRun = useRef(false);
   useEffect(() => {
-    if (DOMRendered) return;
-    setDOMRendered(true);
+    if (hasRun.current) return;
+    hasRun.current = true;
+
     console.log("DOMContentLoaded");
     let telegramScript = document.createElement("script");
     telegramScript.setAttribute("src", "https://telegram.org/js/telegram-widget.js?22");
@@ -20,26 +25,31 @@ function bindComments(telegramPost) {
     let mountCommentsNode = document.getElementById("mountComments");
     mountCommentsNode.parentNode.insertBefore(telegramScript, mountCommentsNode);
     console.log("Telegram script added");
-  })
-  return (<></>);
+  });
+
+  return <div id="mountComments"></div>
+
 }
 
-export default function BlogLayout(props) {
-  const {sidebar, toc, children, ...layoutProps} = props;
+export default function BlogLayout(props: Props): ReactNode {
+  const { sidebar, toc, children, ...layoutProps } = props;
   const hasSidebar = sidebar && sidebar.items.length > 0;
-  // console.log(props);
-  const telegramPost = (
+
+   // console.log(props);
+   const telegramPost: string | undefined = (
     // props.children?.[0].props?.items?.[0]?.content?.frontMatter?.telegram ||
     props.children?.[1]?.props?.children?.type?.frontMatter?.telegram
     // props.children?.[1].props?.children
   );
+
   console.log("Telegram Post Linked", telegramPost);
+
   const comments = telegramPost ? (
     <>
-      <div id="mountComments"></div>
       {bindComments(telegramPost)}
     </>
   ) : null;
+
   return (
     <Layout {...layoutProps}>
       <div className="container margin-vert--lg">
@@ -49,16 +59,16 @@ export default function BlogLayout(props) {
             className={clsx('col', {
               'col--7': hasSidebar,
               'col--9 col--offset-1': !hasSidebar,
-            })}
-            itemScope
-            itemType="http://schema.org/Blog">
+            })}>
             {children}
             {
-              ExecutionEnvironment.canUseDOM && location.href.endsWith("blog") ? null : (
-                <div className={clsx(styles.telegramComments)}>
-                  {comments}
-                </div>
-              )
+              ExecutionEnvironment.canUseDOM && location.pathname.endsWith("blog")
+                ? null
+                : (
+                  <div className={clsx(styles.telegramComments)}>
+                    {comments}
+                  </div>
+                )
             }
           </main>
           {toc && <div className="col col--2">{toc}</div>}
